f0125b2c134e7e818775422dbf29421fbb1de702
Carina Cristina Teixeira De Souza <carinacristina1809@gmail.com>
2026-03-31 08:56:50 -0300

    Add comments to index.js for better clarity

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-require('dotenv').config()
+require('dotenv').config() // Requeriremento ultilizado para funcionar o codigo dentro do arquivo no Node.js
 
-const express = require('express')
-const cors = require('cors')
-const path = require('path')
+const express = require('express') // Requerimento para criar o servidor
+const cors = require('cors') // Requerimento é ativado para que o front-end acesse ou funcione na API
+const path = require('path') // Requerimento dos caminhos dos diretorios
 
-const app = express()
-const PORT = process.env.PORT || 3001
+const app = express() // Rota onde vai funcionar o servidor 
+const PORT = process.env.PORT || 3001 // número da porta do servidor, onde ela vai rodar
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors()) // Libera o aceeso extreno da API
+app.use(express.json()) // Traduz para que os dados em formanto de JSOM , possam ser entedidos pelo servidor
+app.use(express.static(path.join(__dirname, 'public'))) //Faz com que a programação se torne público
 
-const { ready } = require('./src/database/sqlite')
-const routes = require('./src/routes/index')
-
+const { ready } = require('./src/database/sqlite') // importa os dados dentro do banco de dados
+const routes = require('./src/routes/index') // importa os aquirvos e as rotas que seram utilizadas
+//Bloco para pode execultar a programação 
 ready.then(() => {
-  app.use('/api', routes)
-
+  app.use('/api', routes) // Local que juntas as rotas da API
+  //
   app.get('/teste', (req, res) => {
-    res.json({ mensagem: 'API da Pizzaria funcionando!', status: 'online', porta: PORT })
+    res.json({ mensagem: 'API da Pizzaria funcionando!', status: 'online', porta: PORT }) // Rota teste que vai testa se o sistema está funcionado ou ligado
   })
-
+  //
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    res.sendFile(path.join(__dirname, 'public', 'index.html')) //Caso a rota não seja uma API, o usúario vai automaticamente receber "index.html"
   })
-
+  // Mostra quando o servidor estiver funcionado ou rodado 
   app.listen(PORT, () => {
     console.log('=================================')
     console.log('Servidor rodando na porta ' + PORT)
@@ -32,7 +32,8 @@ ready.then(() => {
     console.log('Front-end: http://localhost:' + PORT)
     console.log('=================================')
   })
+  //Caso ele não funcione , aparecerar a mensagem informando o erro 
 }).catch(err => {
   console.error('Erro ao inicializar banco:', err)
-  process.exit(1)
+  process.exit(1) //Encerra a programação
 })
